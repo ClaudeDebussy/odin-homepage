11a2c39bccb79a350db541ea401c82e3766cebc7
ClaudeDebussy <15807600+ClaudeDebussy@users.noreply.github.com>
2025-12-04 15:52:51 +0500

    implement header, color palette, cloudinary

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import "./styles.css";
 import "./modern-css-reset.css";
+import { Cloudinary } from "@cloudinary/url-gen";
+import { fill } from "@cloudinary/url-gen/actions/resize";
+
+const cloudinary = new Cloudinary({
+  cloud: {
+    cloudName: "claudedebussy",
+  },
+});
+
+// const myImage = cloudinary.image("docs/models");
+// myImage.resize(fill().width(250).height(250));
+// const imgElement = document.createElement("img");
+// document.body.appendChild(imgElement);
+// imgElement.src = myImage.toURL();
