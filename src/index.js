f6c9ddd81398588fcc711c25fa635a75718bded5
ClaudeDebussy <15807600+ClaudeDebussy@users.noreply.github.com>
2025-12-09 09:29:29 +0500

    implement responsive header CSS for tablets

--- src/index.js
+++ src/index.js
@@ -11,26 +11,23 @@ function updateHeader(mql) {
     header.innerHTML = `
     <div class="header-img-container">
       <img
-        src="https://res.cloudinary.com/claudedebussy/image/upload/q_auto/claudedebussyportrait.webp"
+        src="https://res.cloudinary.com/claudedebussy/image/upload/ar_1:1,c_fill/q_auto/claudedebussyportrait.webp"
         alt="Portrait of Claude Debussy in sepia tone"
       />
     </div>
     <h1>Claude Debussy</h1>
-    <h2>About Me</h2>
-    <p>
-      I am Achille-Claude Debussy, a composer who believes that music is not
-      merely a set of rules to be followed, but a colorful, fluid expression
-      of nature and emotion. Often described as the first Impressionist
-      composer, I strive to break free from the rigid structures of
-      traditional harmony. My work seeks to capture fleeting moments,
-      shimmering light, and the subtle rhythms of the natural world. I
-      compose not to resolve chords, but to give pleasure and to liberate
-      the imagination.
-    </p>
-    <div class="socials-container">
-      <i class="devicon-opera-plain"></i>
-      <i class="devicon-composer-line"></i>
-      <i class="devicon-beats-plain"></i>
+    <div class="white-bg">
+      <h2>About Me</h2>
+      <p>
+        I am Achille-Claude Debussy, a composer who believes that music is not
+        merely a set of rules to be followed, but a colorful, fluid expression
+        of nature and emotion. Often described as the first Impressionist
+        composer, I strive to break free from the rigid structures of
+        traditional harmony. My work seeks to capture fleeting moments,
+        shimmering light, and the subtle rhythms of the natural world. I
+        compose not to resolve chords, but to give pleasure and to liberate
+        the imagination.
+      </p>
     </div>
     `;
   } else {
